5affa325d23be6bbb18522c70ee7ee72e4cfea66
SoniaFlores1 <floressonia722@gmail.com>
2022-02-09 19:56:33 -0700

    working

--- Exams_FLORES/Exam_1/Exam_1_FLORES.R
+++ Exams_FLORES/Exam_1/Exam_1_FLORES.R
@@ -2,3 +2,12 @@ library(tidyverse)
 
 #I: Read the cleaned_covid_data.csv file into an R Data Frame
 
+clean_cov<- read.csv("cleaned_covid_data.csv")
+class(clean_cov)
+clean_cov %>% class()
+summary(clean_cov)
+
+#II; Subset the data set to just show states that begin with “A” 
+#and save this as an object called A_states.
+
+grepl(pattern="A",clean_cov[,1])
