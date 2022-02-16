1e2cc72bc1766ce3b39d4561a640ae4e60ffa9ed
SoniaFlores1 <floressonia722@gmail.com>
2022-02-16 13:57:09 -0700

    Some corrections to notes

--- week6_notes.R
+++ week6_notes.R
@@ -2,14 +2,14 @@ library(tidyverse)
 
 #message from Dr. Zahn
 
-data.frame(t=seq(-pi,0,.001),
+data_frame(t=seq(-pi, 0, .001),
            x1=16*(sin(t))^2,
-           x2=x1,
-           y=13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t)) %>% 
-  gather(side,x,x1,x2) %>% 
-  ggplot(aes(x,y))+
-  geom_polygon(fill="red")+
-  coord_fixed()+
+           x2=-x1,
+           y=13*cos(t) -5 * cos(2*t) -2*cos(3*t) - cos(4*t)) %>%
+  gather(side, x, x1, x2) %>%
+  ggplot(aes(x,y)) +
+  geom_polygon(fill="red") +
+  coord_fixed() +
   theme_minimal()
 
 table1 #This table is in tidy format. Every column is a variable and rows
