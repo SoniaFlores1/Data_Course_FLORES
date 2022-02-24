ae5441fa15e5f7382ad9c7e8ebd6727209022f76
SoniaFlores1 <floressonia722@gmail.com>
2022-02-24 11:10:00 -0700

    Model notes

--- Class Notes on R/week7_notes.R
+++ Class Notes on R/week7_notes.R
@@ -1,25 +1,6 @@
 library(tidyverse)
 
-##Normal Distribution####
-
-# make tidy dataframe of normal distribution sampling efforts
-
-trials <- list()
-j<- 1
-for(i in c(10, 50, 100, 250, 1000, 3000, 5000, 10000, 100000)){
-  trials[[j]] <- data.frame(x=rnorm(i), y=factor(i))
-  j <- j+1
-}
-
-
-#Bionomial Distribution
-
-trials <- list()
-j<- 1
-
-
-#probability of getting 2 heads out of 2 coin tosses
-dbinom(x=2, size=2, prob=0.5)
+library(tidyverse)
 
 library(palmerpenguins)
 #Models####
@@ -138,6 +119,126 @@ data.frame(cyl=40, trans="manual(m5)", drv="f") %>%
 #Also look at hyp_testing_intro for learning hypothesis testing.
 ###SERIOUSLY PLEASE LOOK AT THIS!!! #
 
+library(tidyverse)
+library(modelr)
+library(easystats)
+library(GGally)
 
+#plot city mpg
+df<-mpg
+
+df %>% names()
+
+ggpairs(df, columns = c("cty","displ","drv"))
+
+df %>% 
+  ggplot(aes(x=displ, y=cty, color=drv)) + 
+  geom_point()
+#this model uses two predicting variables for city mpg
+
+mod1<- glm(data=df,
+           formula= cty~ displ) #displacement on cty
+
+mod2<- glm(data=df,
+           formula= cty ~ displ + drv) #displacement and drive on cty
+
+mod3<- glm(data=df,
+           formula= cty~ displ * drv) #displacement and drive on cty where
+                                     #displacement varies with drive
+
+summary(mod1)
+#slope is -2.6, y-intercept is 26. 
+summary(mod2)
+#slope is -2.33, y-intercept is 23.6. for front-wheel drive, 2.27 is added.
+           #for rear-wheel, 2.5 is added.
+summary(mod3)
+
+
+compare_models(mod1, mod2, mod3, style="se_p")
+#in mod 3, rear_wheel drive drops in significance.
+#intercept changes in all of them. Which one is the true intercept?
+     #(Technically none of them, but some models have a better approximation)
+compare_performance(mod1,mod2,mod3)
+#Mod3 has a better R^2, but about 26% of the data can not be explained by the model.
+#Root-Mean-Square Error predicts how far from reality our predicted line is,
+#and all the models are close to each other in score.
+
+compare_performance(mod1,mod2,mod3) %>% plot() #graphical comparison
+
+#look at a model predictions for a hypothetical car.
+hyp_preds<- data.frame(displ=6, drv ="r") %>% 
+  gather_predictions(mod1,mod2,mod3)
+#if the car has a displacement of 6 and rear-wheel drv, what are its miles per gallon
+hyp_preds
+#all models are giving us different predictions for what the mpg should be.
+
+gather_predictions(data=df, mod1,mod2,mod3) %>% 
+  ggplot(aes(x=displ, y=cty, color=drv)) +
+  geom_point()+
+  geom_smooth(method="lm", aes(y=pred), se=FALSE)+
+  facet_wrap(~model)+
+  geom_point(data= hyp_preds, aes(x=displ, y=pred),color="Black")
+
+#revisit our model formulae
+mod1$formula
+mod2$formula
+mod3$formula
+
+#closer look at interactions
+#interactons####
+
+sim3
+#y is response, x1 and x2 are predictors
+mod1<-glm(data=sim3,
+    formula= y~ x1 + x2) #without interaction
+
+mod2<-glm(data=sim3,
+    formula= y~ x1 * x2) #with interaction
+summary(mod1)
+summary(mod2)
+
+compare_models(mod1,mod2)
+compare_performance(mod1,mod2)
+compare_performance(mod1,mod2) %>% plot()
+
+sim3 %>% 
+  gather_predictions(mod1,mod2) %>% 
+  ggplot(aes(x=x1, y=pred, color=x2))+
+  geom_smooth(method="lm")+
+  facet_wrap(~model)
+
+sim3 %>% 
+  ggplot(aes(x=x1,y=y,color=x2))+
+  geom_point()
+
+library(MASS)
+mpg %>% names
+mod3<- glm(data=mpg,
+           formula=cty ~ displ * year * cyl * drv * model)
+
+step<- stepAIC(mod3)   #stepAIC will help with removing variables from 
+                        #complicated models
+
+
+step$formula
+
+mod4<-glm(data=mpg,
+          formula=step$formula)
+
+add_predictions(mpg,mod4) %>% 
+  ggplot(aes(x=displ,y=pred, color=factor(cyl)))+
+  geom_smooth(method="lm")
+
+mpg %>% 
+  ggplot(aes(x=displ, y=cty))+
+  geom_point()
+
+mod5<- glm(data=mpg,
+           formula= cty~ poly(displ,2)) #second order polynomial
+
+add_predictions(mpg,mod5) %>% 
+  ggplot(aes(x=displ))+
+  geom_point(aes(y=cty))+
+  geom_smooth(aes(y=pred))
 
 
