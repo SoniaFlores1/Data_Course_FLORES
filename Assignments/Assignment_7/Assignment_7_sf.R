af69fe9de4750c25617bb2c4b7bf2061a5b158e7
SoniaFlores1 <floressonia722@gmail.com>
2022-03-02 20:13:43 -0700

    .

--- Assignments/Assignment_7/Assignment_7_sf.R
+++ Assignments/Assignment_7/Assignment_7_sf.R
@@ -37,3 +37,77 @@ Flights %>%
   geom_smooth()
 #5. Plot effect of snowfall on departure and arrival delays ________
   
+
+
+
+
+
+
+library(tidyverse)
+library(patchwork)
+
+# set abs file path to Assignment_7
+path <- "/home/gzahn/Desktop/GIT_REPOSITORIES/gzahn.github.io/data-course/Repository/Assignments/Assignment_7"
+
+# find the csv files
+files <- list.files(path,pattern = ".csv",full.names = TRUE)
+
+# read them into a list
+dfs <- map(files,read_csv)
+
+# name the list elements by the csv filename
+names(dfs) <- basename(files) %>% str_remove_all(".csv") %>% janitor::make_clean_names()
+names(dfs)
+
+# fix names in snowfall data set to match "IATA_CODE"
+names(dfs$jan_snowfall) <- c("Date","IATA_CODE","snow_precip_mm")
+
+# make "Date" column in flights; subset to necessary columns
+dfs$jan_flights <- dfs$jan_flights %>% 
+  mutate(Date=paste0(YEAR,"-",MONTH,"-",DAY) %>% as.Date()) %>% 
+  select(c("Date","ORIGIN_AIRPORT","DESTINATION_AIRPORT","DEPARTURE_DELAY","ARRIVAL_DELAY"))
+
+# join airports and snowfall; select IATA_CODE, Date, snow_precip_mm
+dfs$weather <- full_join(dfs$airports,dfs$jan_snowfall) %>% 
+  select(IATA_CODE,Date,snow_precip_mm)
+
+# create df for departure delays and weather (rename ORIGIN_AIRPORT to IATA_CODE to pull in daily weather info)
+departures <- dfs$jan_flights %>% 
+  rename(IATA_CODE=ORIGIN_AIRPORT) %>% 
+  select(-ARRIVAL_DELAY) %>% 
+  full_join(dfs$weather) %>% 
+  select(-c(IATA_CODE,DESTINATION_AIRPORT)) %>% 
+  rename(DEPARTURE_SNOW=snow_precip_mm)
+
+
+# create df for arrival delays and weather (rename DESTINATION_AIRPORT to IATA_CODE to pull in daily weather info)
+arrivals <- dfs$jan_flights %>% 
+  rename(IATA_CODE=DESTINATION_AIRPORT) %>% 
+  select(-DEPARTURE_DELAY) %>% 
+  full_join(dfs$weather) %>% 
+  select(-c(IATA_CODE,ORIGIN_AIRPORT)) %>% 
+  rename(ARRIVAL_SNOW=snow_precip_mm)
+
+# remove data.frame list to free up space
+rm(dfs)
+
+
+# departure delay vs snow at departure airport
+departures %>% 
+  ggplot(aes(x=DEPARTURE_SNOW,y=DEPARTURE_DELAY)) +
+  geom_smooth()
+
+# delays correlation
+
+departures$Type <- "Departure"
+arrivals$Type <- "Arrival"
+
+
+data.frame(delay=c(departures$DEPARTURE_DELAY,arrivals$ARRIVAL_DELAY),
+           type=c(departures$Type,arrivals$Type),
+           date=c(departures$Date,arrivals$Date)) %>% 
+  ggplot(aes(x=date,y=delay,color=type)) +
+  geom_smooth()
+
+
+
